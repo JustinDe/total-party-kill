e0ec7b262e1fc01550b2a49d468cd4c179d7f873
Phil Hoyt <phil@philhoyt.com>
2015-01-24 22:31:43 -0500

    mo code mo problems

--- js/combatState.js
+++ js/combatState.js
@@ -8,11 +8,11 @@ for(var i = 0; i <= currentHeros.length -1; i++)
 		document.getElementById("MemberSlot"+(i+1)).classList.remove("PartyMemberOff");
 		
 		var MemberUl = $('#MemberSlot'+ (i+1)).find('ul');
-	    $(MemberUl).append("<li onclick='call_attack(linkingHero[currentHeros[i]].Attack1)'>" + linkingHero[currentHeros[i]].Attack1[0] + "</li>");
-	    $(MemberUl).append("<li onclick='call_attack(linkingHero[currentHeros[i]].Attack2)'>" + linkingHero[currentHeros[i]].Attack2[0] + "</li>");
-	    $(MemberUl).append("<li onclick='call_attack(linkingHero[currentHeros[i]].Special1)'>" + linkingHero[currentHeros[i]].Special1[0] + "</li>");
-	    $(MemberUl).append("<li onclick='call_attack(linkingHero[currentHeros[i]].Special2)'>" + linkingHero[currentHeros[i]].Special2[0] + "</li>");
-	    $(MemberUl).append("<li onclick='call_attack(linkingHero[currentHeros[i]].RunAway)'>" + linkingHero[currentHeros[i]].RunAway[0] + "</li>");
+	    $(MemberUl).append("<li id='Attack1-"+currentHeros[i]+"' onclick='call_attack(linkingHero[" + currentHeros[i] + "].Attack1);'>" + linkingHero[currentHeros[i]].Attack1[0] + "</li>");
+	    $(MemberUl).append("<li id='Attack2-"+currentHeros[i]+"' onclick='call_attack(linkingHero[" + currentHeros[i] + "].Attack2);'>" + linkingHero[currentHeros[i]].Attack2[0] + "</li>");
+	    $(MemberUl).append("<li id='Special1-"+currentHeros[i]+"' onclick='call_attack(linkingHero[" + currentHeros[i] + "].Special1);'>" + linkingHero[currentHeros[i]].Special1[0] + "</li>");
+	    $(MemberUl).append("<li id='Special2-"+currentHeros[i]+"' onclick='call_attack(linkingHero[" + currentHeros[i] + "].Special2);'>" + linkingHero[currentHeros[i]].Special2[0] + "</li>");
+	    $(MemberUl).append("<li id='RunAway-"+currentHeros[i]+"' onclick='call_attack(linkingHero[" + currentHeros[i] + "].RunAway);'>" + linkingHero[currentHeros[i]].RunAway[0] + "</li>");
 	}
 	else
 	{
@@ -21,19 +21,50 @@ for(var i = 0; i <= currentHeros.length -1; i++)
 
 }
 
-for(var p = 0; p < 3; p++)
+for(var i = 0; i < 3; i++)
 {
-	if(currentMonsters[p] != 0)
+	if(currentMonsters[i] != 0)
 	{
-		document.getElementById("EnemySlot"+(p+1)).style.backgroundImage="url("+ linkingMonster[currentMonsters[p]].Portrait +")";
-		document.getElementById("EnemySlot"+(p+1)).classList.remove("PartyMemberOff");
-		var EnemyDiv = $('#EnemySlot'+ (p+1));
-		$(EnemyDiv).append("<div class='enemyAction' onclick='attack_monster(linkingMonster[currentMonsters[p]])'></div>");
+		document.getElementById("EnemySlot"+(i+1)).style.backgroundImage="url("+ linkingMonster[currentMonsters[i]].Portrait +")";
+		document.getElementById("EnemySlot"+(i+1)).classList.remove("PartyMemberOff");
+		var EnemyDiv = $('#EnemySlot'+ (i+1));
+		$(EnemyDiv).append("<div class='enemyAction' onclick='attack_monster(linkingMonster["+currentMonsters[i]+"]);'></div>");
 
 	}
-	if(currentMonsters[p] == "")
+	if(currentMonsters[i] == "")
 	{
-		document.getElementById("EnemySlot"+(p+1)).classList.add("PartyMemberOff");
+		document.getElementById("EnemySlot"+(i+1)).classList.add("PartyMemberOff");
 	}
 
+}
+
+function updateUI()
+{
+	for(var i = 0; i <= currentHeros.length -1; i++)
+	{
+		if(linkingHero[currentHeros[i]][1] == false)
+		{
+			document.getElementById("Attack1-"+currentHeros[i]).classList.add("Used");
+		}
+		if(linkingHero[currentHeros[i]][1] == false)
+		{
+			document.getElementById("Attack2-"+currentHeros[i]).classList.add("Used");
+		}
+		if(linkingHero[currentHeros[i]][1] == false)
+		{
+			document.getElementById("Special1-"+currentHeros[i]).classList.add("Used");
+		}
+		if(linkingHero[currentHeros[i]][1] == false)
+		{
+			document.getElementById("Special2="+currentHeros[i]).classList.add("Used");
+		}
+		if(linkingHero[currentHeros[i]][1] == false)
+		{
+			document.getElementById("RunAway-"+currentHeros[i]).classList.add("Used");
+		}
+
+		//var MemberUlLi = $('#MemberSlot'+ (i+1)).find('ul');
+		//console.log(MemberUlLi);
+		//$MemberUlLi.addClass("Used");
+	}
 }
--- js/combat_engine.js
+++ js/combat_engine.js
@@ -11,15 +11,19 @@ function call_attack(attack_key){
     if(attack_key[1] == false){
         //already_used();
         alert("This move has been used. Pick a different one");
+        $(attack_key[1]).addClass('Used');
         attack_ready = false
     }
     else if(attack_to_use[0] == "Save Yourself"){
         //This character exits the battle
         console.log("Save Yourseeeeeeeeeelf");
+        attack_to_use[1] = false;
+        updateUI();
         attack_ready = false
     }
     else if(attack_to_use[0] == "Cleave"){
             //Do 1 damage to all enemies
+
             console.log(linkingMonster[currentMonsters[0]]);
             console.log(linkingMonster[currentMonsters[1]]);
             console.log(linkingMonster[currentMonsters[2]]);
@@ -32,7 +36,10 @@ function call_attack(attack_key){
             if(linkingMonster[currentMonsters[2]] != undefined){
                 linkingMonster[currentMonsters[2]].HP -= 1;
             }
+            attack_to_use[1] = false;
+            updateUI();
             attack_ready = false
+            monster_attacks_you();
             //console.log("Cleave");
     }
 }
@@ -40,19 +47,22 @@ function call_attack(attack_key){
 function attack_monster(monster_to_attack){
     if(attack_ready){
         attack_to_use[1] = false;
+        updateUI();
         if(attack_to_use[0] == "Strike" 
         || attack_to_use[0] == "Jab" 
         || attack_to_use[0] == "Magic Arrow"){
             //Do 1 damage to an enemy
             monster_to_attack.HP -= 1;
             console.log("Physical Attack");
+            console.log("Remaining HP: " + monster_to_attack.HP)
         }
         
         else if(attack_to_use[0] == "Shield Bash"){
             //Do 1 damage to an enemy and block next attack
             monster_to_attack.HP -= 1;
             block_next_move = true;
-            console.log("Shield Bash");   
+            console.log("Shield Bash");
+            console.log("Remaining HP: " + monster_to_attack.HP)   
         }
         else if(attack_to_use[0] == "Smoke"){
             //Battle is over if you're a bitch
@@ -62,48 +72,72 @@ function attack_monster(monster_to_attack){
             //Do 2 damage to whoever
             monster_to_attack.HP -= 2;
             console.log("Poison");
+            console.log("Remaining HP: " + monster_to_attack.HP)
         }
         else if(attack_to_use[0] == "Fireball"){
             //0 damage to fire enemy, 2 damage to ice enemy
             if(monster_to_attack.Element == "Ice"){
                 monster_to_attack.HP -=2;
                 console.log("Fireball on ice enemy");
+                console.log("Remaining HP: " + monster_to_attack.HP)
             }
             else if(monster_to_attack.Element == "None"){
                monster_to_attack.HP -=1;
                console.log("Fireball on normal enemy");
+               console.log("Remaining HP: " + monster_to_attack.HP)
+            }
+            else{
+                console.log("Fireball on fire enemy");
+                console.log("Remaining HP: " + monster_to_attack.HP)
             }
         }
         else if(attack_to_use[0] == "Blizzard"){
             //2 damage to fire enemy, 0 damage to ice enemy
             if(monster_to_attack.Element == "Fire"){
                 monster_to_attack.HP -=2;
-
+                console.log("Remaining HP: " + monster_to_attack.HP)
+                console.log("Blizzard on fire enemy");
             }
             else if(monster_to_attack.Element == "None"){
                 monster_to_attack.HP -=1;
+                console.log("Remaining HP: " + monster_to_attack.HP)
+                console.log("Blizzard on normal enemy");
+            }
+            else{
+                console.log("Remaining HP: " + monster_to_attack.HP)
+                console.log("Blizzard on ice enemy");
             }
         }
+        monster_attacks_you();
         attack_ready = false;
+        if(monster_to_attack.HP <= 0){
+            monster_to_attack = "";
+            /*console.log(monster_to_attack);
+            console.log(currentMonsters);
+            console.log("Monster should be dead");*/
+        }
     }
 }
 
 function monster_attacks_you(){
+    var leave_loop = false;
     if(block_next_move){
         block_next_move = false;
     }
     else{
-        var randHero = Math.floor(Math.random() * 5  ) + 1; 
+        var randHero = Math.floor(Math.random() * 5  ); 
         while(linkingHero[currentHeros[randHero]] == 0){
             randHero = Math.floor(Math.random() * 5  ) + 1;
         }
-        
-        while(true){
+        console.log("Hero hit:" + randHero);
+        while(leave_loop == false){
             var randAttack = Math.floor(Math.random() * 5  ) + 1;
             switch(randAttack){
                 case 1:
                     if(linkingHero[currentHeros[randHero]].Attack1[1] != false){
                         linkingHero[currentHeros[randHero]].Attack1[1] = false;
+                        leave_loop = true;
+                        break;
                     }
                     else{
                         break;
@@ -111,6 +145,8 @@ function monster_attacks_you(){
                 case 2:
                     if(linkingHero[currentHeros[randHero]].Attack2[1] != false){
                         linkingHero[currentHeros[randHero]].Attack2[1] = false;
+                        leave_loop = true;
+                        break;
                     }
                     else{
                         break;
@@ -118,6 +154,8 @@ function monster_attacks_you(){
                 case 3:
                     if(linkingHero[currentHeros[randHero]].Special1[1] != false){
                         linkingHero[currentHeros[randHero]].Special1[1] = false;
+                        leave_loop = true;
+                        break;
                     }
                     else{
                         break;
@@ -125,6 +163,8 @@ function monster_attacks_you(){
                 case 4:
                     if(linkingHero[currentHeros[randHero]].Special2[1] != false){
                         linkingHero[currentHeros[randHero]].Special2[1] = false;
+                        leave_loop = true;
+                        break;
                     }
                     else{
                         break;
@@ -132,24 +172,17 @@ function monster_attacks_you(){
                 case 5:
                     if(linkingHero[currentHeros[randHero]].RunAway[1] != false){
                         linkingHero[currentHeros[randHero]].RunAway[1] = false
+                        leave_loop = true;
+
+                        break;
                     }
                     else{
                         break;
                     }
                 }
-            
         }
+        console.log("now hero " + randHero + " can't use attack " + randAttack)
     }
-}
-
-function count_heros(){
-    var counter=0;
-    for(i = 0; i < 5; i++){
-        if(linkingHero[currentHeros[i]] != 0){
-            counter += 1;
-        }
-    }
-    return counter;
 }
 
 /*console.log(linkingMonster[currentMonsters[0]].HP);
